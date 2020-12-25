d2d861be87ae92fa7507466c7991e2e4b8c9997e
Sai kumar <saik2409@outlook.com>
2020-12-25 10:50:07 +0530

    Updated the signup page

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+var fristName = req.body.fName;
+var lastName = req.body.lName;
+var email = req.body.email;
+
+  console.log(fristName, lastName, email);
+})
+
+app.listen(3000, function(){
+  console.log("Server 3000");
+});
